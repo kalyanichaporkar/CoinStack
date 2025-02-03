e20ddd063744f105c7144683d6ea6d656efa1f8b
unknown <chaporkar2010@gmail.com>
2025-02-03 23:12:05 +0530

    Front-end

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,23 @@ import './App.css';
 import React from 'react';
 import Login from './Login';
 import Homepage from './Homepage';
-import Registration from './Registration';
-import ApiConnectivity from './ApiConnectivity';
-import Portfolio from './Portfolio';
-import Stockdetails from './Stockdetails';
+// import Registration from './Registration';
+// import ApiConnectivity from './ApiConnectivity';
+// import Portfolio from './Portfolio';
+// import Stockdetails from './Stockdetails';
+import { Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
     <div>
-      <Homepage/>
-      <Portfolio/>
-      <Login/> 
-      <Registration/>
-      <ApiConnectivity/>
-      <Stockdetails/>
+      <Routes><Route path='' element={Homepage}></Route>
+      <Route path='/Login' element={Login}></Route>
+      
+      
+      </Routes>
+
+    
       
     </div>
   );
